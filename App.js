852988221d0872245e9113263a8bc29a06aed147
Kenneth <keennetk@ntnu.no>
2020-03-05 17:50:53 +0100

    design test

--- App.js
+++ App.js
@@ -1,15 +1,56 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-import React from 'react';
-import Inputs from './inputs.js';
+const styles = StyleSheet.create({
+  order: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    alignSelf: 'center',
+  },
+  FFT: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: 'yellow',
+    alignSelf: 'center',
+  },
+  meny: {
+    color: 'black',
+    fontSize: 25,
+    alignSelf: 'flex-end',
+  },
+});
 
-const App = () => {
-  return <Inputs />;
-};
-export default App;
+export default class AlignItemsBasics extends Component {
+  render() {
+    return (
+        <View
+            style={{
+              flex: 1,
+              flexDirection: 'column',
+              alignItems: 'stretch',
+            }}>
+          <View
+              style={{
+                flex: 0.15,
+                backgroundColor: 'steelblue',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}>
+            <Text style={styles.FFT}>Fast Track Taxi</Text>
+            <Text style={styles.meny}>Menu</Text>
+          </View>
+
+          <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+          <View
+              style={{
+                flex: 0.3,
+                backgroundColor: 'steelblue',
+              }}>
+            <Text style={styles.order}> Besill Taxi</Text>
+            <Text style={styles.order}>Pris 30kr</Text>
+          </View>
+        </View>
+    );
+  }
+}
